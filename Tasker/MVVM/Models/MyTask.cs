7faa66c9b3e9a738b678819ebed6f41b422b7c17
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-07-22 08:38:48 -0600

    Added ColorConverter

---
+++ Tasker/MVVM/Models/MyTask.cs
@@ -0,0 +1,13 @@
+﻿namespace Tasker.MVVM.Models
+{
+    public class MyTask
+    {
+        public int CategoryId { get; set; }
+
+        public string TaskName { get; set; }
+
+        public bool Completed { get; set; }
+
+        public string TaskColor { get; set; }
+    }
+}
--- Tasker/MVVM/ViewModels/MainViewModel.cs
+++ Tasker/MVVM/ViewModels/MainViewModel.cs
@@ -1,10 +1,4 @@
-﻿using CoreVideo;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 using Tasker.MVVM.Models;
 
 namespace Tasker.MVVM.ViewModels
@@ -18,10 +12,13 @@ namespace Tasker.MVVM.ViewModels
 
         public ObservableCollection<Category> Categories { get; set; }
 
+        public ObservableCollection<MyTask> Tasks { get; set; }
+
+
         private void FillData()
         {
             Categories = new ObservableCollection<Category>
-               {
+            {
                     new Category
                     {
                          Id = 1,
@@ -40,7 +37,88 @@ namespace Tasker.MVVM.ViewModels
                          CategoryName = "Shopping",
                          Color = "#14df80"
                     }
-               };
-            }
+            };
+
+
+
+            Tasks = new ObservableCollection<MyTask>
+            {
+                    new MyTask
+                    {
+                         TaskName = "Upload exercise files",
+                         Completed = false,
+                         CategoryId = 1
+                    },
+                    new MyTask
+                    {
+                         TaskName = "Plan next course",
+                         Completed = false,
+                         CategoryId = 1
+                    },
+                    new MyTask
+                    {
+                         TaskName = "Upload new ASP.NET video on YouTube",
+                         Completed = false,
+                         CategoryId = 2
+                    },
+                    new MyTask
+                    {
+                         TaskName = "Fix Settings.cs class of the project",
+                         Completed = false,
+                         CategoryId = 2
+                    },
+                    new MyTask
+                    {
+                         TaskName = "Update github repository",
+                         Completed = true,
+                         CategoryId = 2
+                    },
+                    new MyTask
+                    {
+                         TaskName = "Buy eggs",
+                         Completed = false,
+                         CategoryId = 3
+                    },
+                    new MyTask
+                    {
+                         TaskName = "Go for the pepperoni pizza",
+                         Completed = false,
+                         CategoryId = 3
+                    },
+            };
+
+            UpdateData();
         }
+
+        public void UpdateData()
+        {
+            foreach (var cate in Categories)
+            {
+                var tasks = Tasks.Where(t => t.CategoryId == cate.Id)
+                                .ToList();
+
+                var completed = tasks.Where(t => t.Completed == true)
+                                   .ToList();
+
+                var notCompleted = tasks.Where(t => t.Completed == false)
+                                   .ToList();
+
+
+
+                cate.PendingTasks = notCompleted.Count();
+                cate.Percentage = (float)completed.Count() / (float)(tasks.Count() == 0 ? 1 : tasks.Count());
+            }
+
+            foreach (var task in Tasks)
+            {
+                var catColor = Categories.Where(c => c.Id == task.CategoryId)
+                    .Select(c => c.Color)
+                    .FirstOrDefault();
+
+                task.TaskColor = catColor ?? string.Empty; // Ensure TaskColor is never assigned a null value
+            }
+
+        }
+
     }
+}
